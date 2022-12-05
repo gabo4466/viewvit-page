75adf98f44fbbfcc5925c9a3bf0cefbca6855f33
Gabriel <costanzag1999@gmail.com>
2022-12-05 01:18:47 +0100

    SweetAlert2 package && User interface created

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,4 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, type Ref } from "vue";
+import Swal from "sweetalert2";
+import type { User } from "@/interfaces/user.interface";
+
+const isLoading: Ref<boolean> = ref(false);
+
+const user: Ref<User> = ref({
+    email: "",
+    password: "",
+});
+
+function signIn(event: Event) {
+    Swal.fire(`Hola ${user.value.email}`);
+    console.log({ event });
+    isLoading.value = !isLoading.value;
+}
+</script>
 
 <template>
     <div class="surface-card p-4 shadow-2 border-round w-full lg:w-6">
@@ -20,34 +37,52 @@
         </div>
 
         <div>
-            <label for="email1" class="block text-900 font-medium mb-2"
+            <label for="email" class="block text-900 font-medium mb-2"
                 >Email</label
             >
-            <InputText id="email1" type="text" class="w-full mb-3" />
+            <InputText
+                v-model.trim="user.email"
+                id="email"
+                type="text"
+                class="w-full mb-3"
+            />
 
-            <label for="password1" class="block text-900 font-medium mb-2"
+            <label for="password" class="block text-900 font-medium mb-2"
                 >Password</label
             >
-            <InputText id="password1" type="password" class="w-full mb-3" />
+            <InputText
+                v-model.trim="user.password"
+                id="password"
+                type="password"
+                class="w-full mb-3"
+            />
 
             <div class="flex align-items-center justify-content-between mb-6">
-                <div class="flex align-items-center">
-                    <Checkbox
-                        id="rememberme1"
-                        :binary="true"
-                        class="mr-2"
-                    ></Checkbox>
-                    <label for="rememberme1">Remember me</label>
-                </div>
                 <a
                     class="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer"
                     >Forgot password?</a
                 >
             </div>
 
-            <Button label="Sign In" icon="pi pi-user" class="w-full"></Button>
+            <Button
+                @click="signIn"
+                label="Sign In"
+                icon="pi pi-user"
+                class="w-full"
+            ></Button>
+        </div>
+
+        <div class="wrapper w-full">
+            <ProgressSpinner v-show="isLoading" />
         </div>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.wrapper {
+    display: flex;
+    flex-flow: column wrap;
+    align-items: center;
+    padding: 10px 0 0 0;
+}
+</style>
